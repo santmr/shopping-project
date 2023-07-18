a2f0862e4b390e57d88a15ce8363f983b63c6da0
santmr <132427487+santmr@users.noreply.github.com>
2023-07-18 11:43:26 +0530

    Delete shoppingcart directory

--- shoppingcart/src/main/java/com/shoppingcart/application/userrepository/UserAddress.java
+++
@@ -1,7 +0,0 @@
-package com.shoppingcart.application.userrepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAddress extends JpaRepository<UserAddress, Integer>{
-
-}
